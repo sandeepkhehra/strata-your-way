8970e5b724b303dc49870d6f8b143226297fb406
adictonator <adityabhaskarsharma@gmail.com>
2019-05-14 19:27:39 +0530

    removed edit link for maintenance req for users

--- resources/views/shared/users/_maintenance-requests.blade.php
+++ resources/views/shared/users/_maintenance-requests.blade.php
@@ -1,5 +1,5 @@
 @forelse ($maintenanceRequests as $request)
-	<a href="{{ route('maintenance.edit', $request->id) }}" class="list-group-item list-group-item-action flex-column align-items-start">
+	<a href="{{ Auth::user()->type != 0 ? '#' : route('maintenance.edit', $request->id) }}" class="list-group-item list-group-item-action flex-column align-items-start">
 		<div class="d-flex w-100 justify-content-between">
 			<h6 class="mb-1">Maintenance Issue #{{ $request->id }} has been Logged: <br><small>{{ array_search($request->type, $request::TYPES) }}</small></h6>
 			{{-- <h4 class="mb-1">{{ $request->title }}</h4> --}}
